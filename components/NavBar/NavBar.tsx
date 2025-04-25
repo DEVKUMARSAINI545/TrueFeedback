5168ba768543cdc0fb93df378223f068f8022854
dev kumar saini <devsaini27806@gmail.com>
2025-04-25 16:46:53 +0530

    rename sign-in to sign-up

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
         <div className='  px-24   bg-indigo-950  overflow-hidden w-full  h-24 flex justify-between  items-center'>
       <h1 className=' text-2xl font-bold text-white'>True Feedback</h1>
       <p className='text-white text-xl'>{session && "Welcome, "+session?.user?.username}</p>
-       {session? <Button onClick={()=>signOut()} variant="NavBarButton">Logout</Button> : <Button onClick={()=>signIn("/sign-in")} variant="NavBarButton">Login</Button> }
+       {session? <Button onClick={()=>signOut()} variant="NavBarButton">Logout</Button> : <Button onClick={()=>signIn("/sign-up")} variant="NavBarButton">Login</Button> }
    
     </div>
   )
