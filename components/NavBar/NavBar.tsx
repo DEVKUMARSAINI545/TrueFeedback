f8e7340849ffbc6ba7f8b0e36efb4cea1ef8f612
dev kumar saini <devsaini27806@gmail.com>
2025-04-27 09:25:45 +0530

    changed localstorage

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
         <div className='  px-24   bg-indigo-950  overflow-hidden w-full  h-24 flex justify-between  items-center'>
       <h1 className=' text-2xl font-bold text-white'>True Feedback</h1>
       <p className='text-white text-xl'>{session && "Welcome, "+session?.user?.username}</p>
-       {session? <Button onClick={()=>signOut()} variant="NavBarButton">Logout</Button> : <Link href={"/sign-up"}><Button variant="NavBarButton">Login</Button></Link> }
+       {session? <Button onClick={()=>{signOut(),localStorage.removeItem("acceptMessages")}} variant="NavBarButton">Logout</Button> : <Link href={"/sign-up"}><Button variant="NavBarButton">Login</Button></Link> }
    
     </div>
   )
